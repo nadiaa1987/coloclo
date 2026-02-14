60ccc2e2547d1440441b0c3ee2e7c515a197a9b0
DARCY BRIAN JOSEPH <nadiaseddiklaa@gmail.com>
2026-02-14 14:03:34 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://github.

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         hostname: 'raw.githubusercontent.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
