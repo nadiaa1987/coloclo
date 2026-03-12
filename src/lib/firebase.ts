bbfb89c0e8c6e203a381cd0fb6ca31fd4e782b77
MAC <mac@MacBook-Air-de-MAC.local>
2026-03-12 02:08:19 +0100

    Disable Netlify secret scan for Firebase API key

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,8 +7,6 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
